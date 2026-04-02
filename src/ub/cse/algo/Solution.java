95246ca856d222b4fe365c78a4dd5e189cc56674
Eliana <i.eliana.freeman@gmail.com>
2026-04-01 21:28:32 -0400

    started logic to organize priorities. Updating sol.priorities in ascending order since Simulator reverses it

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -2,6 +2,7 @@ package ub.cse.algo;
 
 import java.util.ArrayList;
 
+
 public class Solution {
 
     private Info info;
@@ -29,8 +30,28 @@ public class Solution {
      */
     public SolutionObject outputPaths() {
         SolutionObject sol = new SolutionObject();
-        sol.paths = Traversals.bfsPaths(graph,clients);
-        System.out.print(sol.paths);
+
+        sol.paths = Traversals.bfsPaths(graph, clients);
+        sol.bandwidths = new ArrayList<>(bandwidths);
+
+        //need sol.priorities to organize priorities.
+        //determine order based on payment
+        ArrayList<Client> sortedClients = new ArrayList<>(clients); //based on payments
+        //sort sortedClients
+        //either use sortedClients.sort() if allowed or manually do it. Use client.payment and client.alpha?
+
+
+        //go down in order of priority
+        //if Clients is [client2 = $1,client0 = $30,client1 = $100]
+        //and sortedClients is client1=$100, client0 = $30, client2=$1
+        //then priority would be {index 0: client2, index 1 : client0, index 2 : client1} because simulator has descending order
+        int priority = sortedClients.size();
+        for (Client client : sortedClients) {
+            sol.priorities.put(client.id, priority--);
+        }
+
+
+
         return sol;
 
     }
