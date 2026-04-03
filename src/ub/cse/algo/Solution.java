529f4e409fd25883347ed4d98ca87671970e2fdb
yanchit729 <yanchitchan@gmail.com>
2026-04-03 19:02:07 -0400

    Updated

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -30,31 +30,73 @@ public class Solution {
      */
     public SolutionObject outputPaths() {
         SolutionObject sol = new SolutionObject();
-        sol.paths = Traversals.bfsPaths(graph, clients);
         sol.bandwidths = new ArrayList<>(bandwidths);
+
+        int[] count = new int[bandwidths.size()];
 
         //need sol.priorities to organize priorities.
         //determine order based on payment
         ArrayList<Client> sortedClients = new ArrayList<>(clients); //based on payments
         //sort sortedClients
         //either use sortedClients.sort() if allowed or manually do it. Use client.payment?
-        for (int i = 0; i < sortedClients.size(); i++) {
-            for (int j = i + 1; j < sortedClients.size(); j++) {
-                if (sortedClients.get(i).payment < sortedClients.get(j).payment) {
-                    Client temp = sortedClients.get(i);
-                    sortedClients.set(i, sortedClients.get(j));
-                    sortedClients.set(j, temp);
+        sortedClients.sort((a,b) -> {
+            if (b.payment != a.payment)
+                return b.payment - a.payment;
+            else
+                return Float.compare(a.alpha, b.alpha);
+        });
+
+
+        for (Client c : sortedClients) {
+            PriorityQueue<int[]> todo = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+            int[] dist = new int[graph.size()];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            int[] priors = new int[graph.size()];
+            Arrays.fill(priors, -1);
+
+            int startNode = graph.contentProvider; //Need to locate starting node
+            dist[startNode] = 0;
+            todo.add(new int[]{startNode, 0});
+
+            while (!todo.isEmpty()) {
+                int[] current = todo.poll();
+                int node = current[0];
+                int cost = current[1];
+
+                if (node == c.id)
+                    break;
+
+                if (cost > dist[node])
+                    continue;
+
+                for (Integer n : graph.get(node)) {
+                    int extra = (count[n] + 1)/ bandwidths.get(n);
+                    int newCost = dist[node] + 1 + extra;
+
+                    if (newCost < dist[n]) {
+                        dist[n] = newCost;
+                        priors[n] = node;
+                        todo.add(new int[]{n, newCost});
+                    }
                 }
             }
-        }
 
-        //go down in order of priority
-        //if Clients is [client2 = $1,client0 = $30,client1 = $100]
-        //and sortedClients is client1=$100, client0 = $30, client2=$1
-        //then priority would be {index 0: client2, index 1 : client0, index 2 : client1} because simulator has descending order
-        int priority = sortedClients.size();
-        for (Client client : sortedClients) {
-            sol.priorities.put(client.id, priority--);
+            if (priors[c.id] != -1) {
+                ArrayList<Integer> path = new ArrayList<>();
+                int currentClient = c.id; //start from client
+                //backtrack from client to ISP
+                while (currentClient != startNode) {
+                    path.add(0, currentClient);
+                    currentClient = priors[currentClient];
+                }
+
+                path.add(0, startNode); //add ISP at front
+                sol.paths.put(c.id, path);
+
+                for (int i = 0; i < path.size()-1 ; i++) {
+                    count[path.get(i)]++;
+                }
+            }
         }
 
         return sol;
