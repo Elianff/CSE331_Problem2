59551aea28f78a05a1114179ee00bc682f911df9
yanchit729 <yanchitchan@gmail.com>
2026-04-03 00:21:31 -0400

    Updated

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -1,7 +1,6 @@
 package ub.cse.algo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 
 public class Solution {
@@ -31,28 +30,33 @@ public class Solution {
      */
     public SolutionObject outputPaths() {
         SolutionObject sol = new SolutionObject();
+        sol.paths = Traversals.bfsPaths(graph, clients);
         sol.bandwidths = new ArrayList<>(bandwidths);
 
-        HashMap<Integer, ArrayList<Integer>> path = Traversals.bfsPaths(graph, clients);
-
+        //need sol.priorities to organize priorities.
+        //determine order based on payment
+        ArrayList<Client> sortedClients = new ArrayList<>(clients); //based on payments
+        //sort sortedClients
+        //either use sortedClients.sort() if allowed or manually do it. Use client.payment?
+        for (int i = 0; i < sortedClients.size(); i++) {
+            for (int j = i + 1; j < sortedClients.size(); j++) {
+                if (sortedClients.get(i).payment < sortedClients.get(j).payment) {
+                    Client temp = sortedClients.get(i);
+                    sortedClients.set(i, sortedClients.get(j));
+                    sortedClients.set(j, temp);
+                }
+            }
+        }
 
         //go down in order of priority
         //if Clients is [client2 = $1,client0 = $30,client1 = $100]
         //and sortedClients is client1=$100, client0 = $30, client2=$1
         //then priority would be {index 0: client2, index 1 : client0, index 2 : client1} because simulator has descending order
-
-        for (Client client : clients) {
-            int shortestDelay = info.shortestDelays.get(client.id);
-            if (shortestDelay <= client.alpha) {
-                sol.paths.put(client.id, path.get(client.id));
-                int priority = (int) (client.payment / client.alpha);
-                sol.priorities.put(client.id, priority);
-            }
+        int priority = sortedClients.size();
+        for (Client client : sortedClients) {
+            sol.priorities.put(client.id, priority--);
         }
 
-
-
         return sol;
-
     }
 }
