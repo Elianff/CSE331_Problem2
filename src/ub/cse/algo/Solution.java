bf45b159b5301cb986fd06f7ad80a7ff7ca0f0da
Eliana <i.eliana.freeman@gmail.com>
2026-04-03 19:48:04 -0400

    started comments, will be back to continue in moment

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -28,69 +28,82 @@ public class Solution {
      *
      * @return SolutionObject containing the paths, priorities and bandwidths
      */
+
+    //sources: Day 19 Slides, https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html,https://docs.oracle.com/javase/8/docs/api/java/util/PriorityQueue.html
     public SolutionObject outputPaths() {
         SolutionObject sol = new SolutionObject();
         sol.bandwidths = new ArrayList<>(bandwidths);
 
-        int[] count = new int[bandwidths.size()];
+        int[] count = new int[bandwidths.size()];   //create integer array to store bandwith at each id/index. We don't plan to add more than bandwidths.size
 
         //need sol.priorities to organize priorities.
         //determine order based on payment
         ArrayList<Client> sortedClients = new ArrayList<>(clients); //based on payments
         //sort sortedClients
         //either use sortedClients.sort() if allowed or manually do it. Use client.payment?
+        //https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html:
+        //to sort the clients. If negative, client a is first, and if positive, b
+
         sortedClients.sort((a,b) -> {
-            if (b.payment != a.payment)
+            if (b.payment != a.payment) {
+                //effectively: if b.payment>a.payment, return 1, b goes first
+                //if a.payment>b.payment, return -1, a goes first
+                //simplified b.payment-a.payment
+                //example: b=100 and a=500 then 100-500=-400, a goes first
                 return b.payment - a.payment;
+            }
             else
+                //if it is equal, need tiebreaker to determine which one is first.
                 return Float.compare(a.alpha, b.alpha);
         });
 
-        for (Client c : sortedClients) {
+        for (Client client : sortedClients) {
+            //https://docs.oracle.com/javase/8/docs/api/java/util/PriorityQueue.html
             PriorityQueue<int[]> todo = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-            int[] dist = new int[graph.size()];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            int[] priors = new int[graph.size()];
-            Arrays.fill(priors, -1);
+            int[] distance = new int[graph.size()];
+            Arrays.fill(distance, Integer.MAX_VALUE);      //default fill with really big numbers
+            int[] priority = new int[graph.size()];
+            Arrays.fill(priority, -1);                 //default fill with smallest numbers, update when adding
 
             int startNode = graph.contentProvider; //Need to locate starting node
-            dist[startNode] = 0;
-            todo.add(new int[]{startNode, 0});
+            distance[startNode] = 0;     //distance from startNode to..startNode is 0
+            int[] toQueue = {startNode, 0};    //increased steps to make it clearer, store the node and the cost, least cost prioritized
+            todo.add(toQueue);
 
             while (!todo.isEmpty()) {
-                int[] current = todo.poll();
-                int node = current[0];
+                int[] current = todo.poll();   // take first from queue
+                int node = current[0];         //current looks like {int, int} or {startNode, cost}
                 int cost = current[1];
 
-                if (node == c.id)
+                if (node == client.id)
                     break;
 
-                if (cost > dist[node])
+                if (cost > distance[node])
                     continue;
 
                 for (Integer n : graph.get(node)) {
                     int extra = (count[n] + 1)/ bandwidths.get(n);
-                    int newCost = dist[node] + 1 + extra;
+                    int newCost = distance[node] + 1 + extra;
 
-                    if (newCost < dist[n]) {
-                        dist[n] = newCost;
-                        priors[n] = node;
+                    if (newCost < distance[n]) {
+                        distance[n] = newCost;
+                        priority[n] = node;
                         todo.add(new int[]{n, newCost});
                     }
                 }
             }
 
-            if (priors[c.id] != -1) {
+            if (priority[client.id] != -1) {
                 ArrayList<Integer> path = new ArrayList<>();
-                int currentClient = c.id; //start from client
+                int currentClient = client.id; //start from client
                 //backtrack from client to ISP
                 while (currentClient != startNode) {
                     path.add(0, currentClient);
-                    currentClient = priors[currentClient];
+                    currentClient = priority[currentClient];
                 }
 
                 path.add(0, startNode); //add ISP at front
-                sol.paths.put(c.id, path);
+                sol.paths.put(client.id, path);
 
                 for (int i = 0; i < path.size()-1 ; i++) {
                     count[path.get(i)]++;
